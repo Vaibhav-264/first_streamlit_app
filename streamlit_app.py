2e960ff764827f178aeed10cdf93f60953cbf46e
Vaibhav-264 <132979238+Vaibhav-264@users.noreply.github.com>
2023-05-09 11:33:04 +0530

    Update streamlit_app.py
    Add header "Breakfast Menu"
    and add some text inside it

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Orange, Apple, Banana')
+streamlit.text('Poha and Sambar')
+streamlit.text('Lemon juce')
