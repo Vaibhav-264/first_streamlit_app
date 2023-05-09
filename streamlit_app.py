3eec58dc2a95cda602418a12f58e20585eec18f8
Vaibhav-264 <132979238+Vaibhav-264@users.noreply.github.com>
2023-05-09 11:41:23 +0530

    Update streamlit_app.py
    Add some emoji and make some changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Orange, Apple, Banana')
-streamlit.text('Poha and Sambar')
-streamlit.text('Lemon juce')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
